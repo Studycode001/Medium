8a9770753037b35b0372d57ffebca85cc4ed1255
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2024-10-07 15:57:54 +0530

    just started

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string
+  }
+}>();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -8,7 +15,25 @@ app.get('/', (c) => {
 
 const api = new Hono().basePath('/api/v1')
 
-app.post('/signup', (c) => c.text('Signup page'))
+app.post('/signup', async (c) => {
+  const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      }
+    })
+    return c.text('jwt here')
+  } catch (error) {
+    return c.text('error'),c.status(403);
+  }
+  
+})
 app.post('/signin', (c) => c.text("Post the user login details"))
 app.put('/blog', (c) => c.text("Put or update the details"))
 app.get('/blog/:id', async (c) => {
